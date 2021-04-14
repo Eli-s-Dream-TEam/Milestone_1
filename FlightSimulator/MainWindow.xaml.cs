d365ac248ba572c736a84e402c191680b204f14e
horskyyaron <57840832+horskyyaron@users.noreply.github.com>
2021-04-14 11:06:55 +0300

    rounded corners. can move screen from anywhere by right click and drag. close button upper right corner

--- FlightSimulator/MainWindow.xaml.cs
+++ FlightSimulator/MainWindow.xaml.cs
@@ -54,5 +54,18 @@ namespace FlightSimulator
         {
             this.vm.start();
         }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
     }
 }
